bbed6cf969a856c1c00701bab412f3c5f009f61d
denetz1 <dennis.etzel@gmx.de>
2024-11-02 12:19:09 +0100

    Update 03_project_2_exploratory_data_analysis.sql

--- 03_project_2_exploratory_data_analysis.sql
+++ 03_project_2_exploratory_data_analysis.sql
@@ -9,41 +9,41 @@ FROM layoffs_staging2;
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging2; 
 
-# Now I want to see, what the maximum of the people laid off, and the maximum percentage
+# Now I want to see, what the maximum amount of layoffs as well as the maximum percentage
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2; 
 
-# Next, let's order the companies, which went bankrupt by the funds raised 
+# Next, order the companies, which went bankrupt by the funds raised 
 SELECT*
 FROM layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
-# Now I want to see which company laid off the most people
+# Now I want to see which company had the most layoffs
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC; 
 
-# Next, I want to see which industry laid off the most people
+# Next, I want to see which industry had the most layoffs
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY industry
 ORDER BY 2 DESC;
 
-# Next, I want to see which country laid off the most people
+# Next, I want to see which country had the most layoffs
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
-# I want to get a feeling of in which year the most people were laid off
+# I want to see in which year there were the most layoffs
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 2 DESC;
 
-# And I can look up in what stage of a company the most people were laid off
+# And I can look up in what stage of the company the most people were laid off
 SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY stage
@@ -74,7 +74,7 @@ SELECT
 	SUM(total_off) OVER (ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_Total; 
 
-# I want to look up, which company laid the most people of in each year
+# I want to look up, which company had the most layoffs in each year
 SELECT 
 	company, 
 	YEAR(`date`), 
@@ -83,7 +83,7 @@ FROM layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ORDER BY 3 DESC; 
 
-# And now we can create a ranking of the top5 companies who laid the most people off in each year. 
+# And now I can create a ranking of the top5 companies who had the most layoffs in each year. 
 WITH Company_Year (company, years, total_laid_off) AS (
 	SELECT 
 		company, 
